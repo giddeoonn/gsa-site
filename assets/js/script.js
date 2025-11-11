7752511acf50f683e3a4c59bf1b08db124dd038a
GiDEON <gideon.manalo.gupilan@gmail.com>
2025-11-11 18:20:56 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,44 @@ closebtns.forEach((btn) => {
     closebtns.forEach((btn) => btn.classList.remove("active"));
   });
 });
+
+// SLIDER FUNCTIONALITY
+document.querySelectorAll(".slider").forEach((slider) => {
+  const cards = slider.querySelectorAll(".card");
+  const left = slider.querySelector(".cat-btn.left");
+  const right = slider.querySelector(".cat-btn.right");
+
+  const gap = parseInt(getComputedStyle(cards[0]).marginRight) || 8;
+  const scrollAmount = cards[0].offsetWidth + gap;
+
+  // BUTTON VISIBILITIES
+  const updateButtons = () => {
+    if (slider.scrollLeft <= 0) {
+      left.style.display = "none";
+    } else {
+      left.style.display = "block";
+    }
+
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 1) {
+      right.style.display = "none";
+    } else {
+      right.style.display = "block";
+    }
+  };
+
+  updateButtons();
+
+  slider.addEventListener("scroll", updateButtons); //For SCROLL EVENTS
+
+  // BUTTONS CLICK EVENTS FUNCTIONALITY
+  right.addEventListener("click", () => {
+    slider.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+
+  left.addEventListener("click", () => {
+    slider.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  // RECALCULATE WINDOWS SIZE
+  window.addEventListener("resize", updateButtons);
+});
